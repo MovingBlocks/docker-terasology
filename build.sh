5e6146bf0889c4d0f93a28e9c850cb4cfd1df4ba
Marcel Otte <otte.mm@googlemail.com>
2018-04-03 21:08:16 +0200

    Removing any existing versionInfo.properties now.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm versionInfo.properties*
 
 wget http://jenkins.terasology.org/job/TerasologyStable/lastSuccessfulBuild/artifact/build/resources/main/org/terasology/version/versionInfo.properties
 
